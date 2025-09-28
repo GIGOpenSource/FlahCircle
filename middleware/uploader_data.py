d8433e5c72d802048d9fee15daefcabc9b52151f
liangwater <liangwater@163.com>
2025-09-28 14:31:07 +0800

    上传文件、支付功能

--- middleware/uploader_data.py
+++ middleware/uploader_data.py
@@ -7,10 +7,11 @@ from drf_spectacular.utils import extend_schema, OpenApiParameter
 from .utils import ApiResponse
 
 # 直接在代码中填写AccessKey和Endpoint
-access_key_id = "LTAI5t9kmUmB7C8cRCes5si8"
-access_key_secret = "mD4Ou2icxyoulhyzviDOfLqBsppACC"
-endpoint = "oss-cn-beijing.aliyuncs.com"  # 例如oss-cn-hangzhou.aliyuncs.com
-bucket_name = "flashcircle"
+access_key_id = os.environ.get('ALIYUN_ACCESS_KEY_ID')
+access_key_secret = os.environ.get('ALIYUN_ACCESS_KEY_SECRET')
+endpoint = os.environ.get('ALIYUN_OSS_ENDPOINT', 'oss-cn-beijing.aliyuncs.com')  # 例如oss-cn-hangzhou.aliyuncs.com
+bucket_name = os.environ.get('ALIYUN_OSS_BUCKET_NAME', 'flashcircle')
+
 
 # 初始化OSS客户端
 auth = oss2.Auth(access_key_id, access_key_secret)
