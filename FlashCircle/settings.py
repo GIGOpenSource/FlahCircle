9c37e3365448d2fa20f2275ab989a9e3f2299d7a
liangwater <liangwater@163.com>
2025-10-09 17:35:10 +0800

    检查

--- FlashCircle/settings.py
+++ FlashCircle/settings.py
@@ -152,6 +152,8 @@ else:
         }
     }
 
+print("=============DATABASES=============")
+print(DATABASES)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
